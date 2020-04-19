4cf8a648ca7c26c5a4ee11b3af19541e233245eb
Dyakov Anton <dyakant@gmail.com>
2020-04-19 11:51:53 +0300

    L25. Added loading of Products

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -15,15 +15,19 @@
     <div class="a-spacing-large" />
     <div class="container-fluid browsing-history">
       <div class="row">
-        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb">
+        <div 
+          v-for="(product, index) in products" 
+          :key="product._id"
+          class="col-xl-2 col-lg-2 col-md-3 col-sm-6 col-6 br bb"
+        >
           <div class="history-box">
             <!-- Product image -->
             <a href="#" class="a-link-normal">
-              <img src="#" alt="Here should be a Product image" class="img-fluid">
+              <img :src="product.photo" alt="Here should be a Product image" class="img-fluid">
             </a>
             <div class="a-spacing-top-base asin-title">
               <span class="a-text normal">
-                <div class="p13n-sc-truncated">Product title</div>
+                <div class="p13n-sc-truncated">{{ product.title }}</div>
               </span>
             </div>
             <!-- Product rating -->
@@ -41,7 +45,7 @@
             <!-- Product price -->
             <div class="a-row">
               <span class="a-size-base a-color-price">
-                <span class="p13n-sc-price">$23</span>
+                <span class="p13n-sc-price">${{ product.price }}</span>
               </span>
             </div>
             <!-- Product buttons -->
@@ -65,7 +69,7 @@
     }) { // TODO What is it?
       try {
         let response = await $axios.$get("http://localhost:3000/api/products");
-        console.log(response);
+        console.log('Products have been loaded;');
         return {
           products: response.products
         };
